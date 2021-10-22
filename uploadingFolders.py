b7f9f56a126ea5ecf0371072a57c9175b56af617
banana7045 <70290401+banana7045@users.noreply.github.com>
2021-10-21 22:07:20 -0400

    Add files via upload

---
+++ uploadingFolders.py
@@ -0,0 +1,23 @@
+import os
+import dropbox
+from dropbox.files import WriteMode
+class TransferData:
+    def __init__(self,access_token):
+        self.access_token =  access_token
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+        for root, dirs, files in os.walk(file_from):
+            for filename in files:
+                local_path = os.path.join(root, filename)
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+def main():
+    access_token = 'sl.A61oCeQZ4UcybWv-78I3nBdrAequZF73Yl4tbhBhuNu0L-NIyr8ea5aDeYc8MvGQnb1nthhNvIDke1SMHaezR3VHErNS5QygaY84B5izw_Y2-xLdfbRq-5Uv0oN83f686jaifFw'
+    transferData = TransferData(access_token)
+    file_from = str(input("Input the folder's path (the one you want to transfer): "))
+    file_to = input("Input the full file/folder path to upload to dropbox") 
+    transferData.upload_file(file_from,file_to)
+    print("The file was moved.")
+main()
